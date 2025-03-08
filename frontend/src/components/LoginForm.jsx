7479b7767990f735b878a144950b97dded583a99
mariyancholakov <m.cholakov09@gmail.com>
2025-03-08 22:17:30 +0200

    Added functionality to the login and register forms

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,16 +1,47 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function LoginForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const res = await fetch("http://localhost:5173/api/auth/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+      });
+      if (!res.ok) {
+        console.log("Couldn't login user!");
+        setLoading(false);
+      }
+      const data = await res.json();
+      localStorage.setItem("token", data.token);
+      setUsername("");
+      setPassword("");
+      setLoading(false);
+      navigate("/");
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <div className="border w-86 h-[60%] px-4 py-5 flex flex-col gap-4">
       <h2 className="text-lg font-semibold text-center">Login Form</h2>
-      <form className="flex flex-col gap-6">
+      <form onSubmit={handleLogin} className="flex flex-col gap-6">
         <input
           className="py-3 pl-4 border w-full rounded-sm "
           type="text"
           name="username"
           placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
 
         <input
@@ -18,13 +49,16 @@ function LoginForm() {
           type="password"
           name="password"
           placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
         <button
+          disabled={loading}
           className="py-3 pl-4 bg-indigo-500 hover:bg-indigo-400 transition duration-300 ease-in-out cursor-pointer text-center text-white text-lg font-semibold w-full rounded-sm "
           type="submit"
         >
-          Login
+          {loading ? "Submitting..." : "Login"}
         </button>
       </form>
       <p className="text-slate-700 text-center">
--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,16 +1,47 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 function RegisterForm() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const res = await fetch("http://localhost:5173/api/auth/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, email, password }),
+      });
+      if (!res.ok) {
+        console.log("Couldn't register user!");
+        setLoading(false);
+      }
+      setUsername("");
+      setEmail("");
+      setPassword("");
+      setLoading(false);
+      navigate("/api/auth/login");
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <div className="border w-86 h-[60%] px-4 py-5 flex flex-col gap-4">
       <h2 className="text-lg font-semibold text-center">Register Form</h2>
-      <form className="flex flex-col gap-6">
+      <form onSubmit={handleRegister} className="flex flex-col gap-6">
         <input
           className="py-3 pl-4 border w-full rounded-sm "
           type="text"
           name="username"
           placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
 
         <input
@@ -18,6 +49,8 @@ function RegisterForm() {
           type="email"
           name="email"
           placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
 
         <input
@@ -25,13 +58,16 @@ function RegisterForm() {
           type="password"
           name="password"
           placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
         <button
+          disabled={loading}
           className="py-3 pl-4 bg-indigo-500 hover:bg-indigo-400 transition duration-300 ease-in-out cursor-pointer text-center text-white text-lg font-semibold w-full rounded-sm "
           type="submit"
         >
-          Register
+          {loading ? "Submitting" : "Register"}
         </button>
       </form>
       <p className="text-slate-700 text-center">
