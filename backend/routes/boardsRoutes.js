daa96a7a83405fb652b42cabc515d9c21d4d2a82
Eva <e.ivaylova@gmail.com>
2025-03-15 19:11:25 +0200

    Fixed all board and post methods. I think this is it.

--- backend/routes/boardsRoutes.js
+++ backend/routes/boardsRoutes.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import mongoose from "mongoose";
 import { Board } from '../models/Boards.js';
-import {Post} from "../models/Posts.js";
 const boardRouter = express.Router();
 
 
