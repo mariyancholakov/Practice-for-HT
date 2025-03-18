28c06431aa91916c51c72816bf332ce1474866d5
Stellusha <stellamarkova09@gmail.com>
2025-03-18 23:02:52 +0200

    nabashevakatalojnitedanni

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -3,9 +3,15 @@ import NavBar from './NavBar'
 
 const MainPage = () => {
   return (
-    <div className='bg-color-black'>
+    <div className='bg-black min-h-screen'>
       <NavBar />
-      
+      <div className='bg-black p-4 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4'>
+        {images.map((src, index) => (
+          <div key={index} className='overflow-hidden rounded-lg shadow-lg'>
+            
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
